bc2f66be3ce0fd2691637c4d55d4d0fa2f675b76
Trần Duy Thanh <coachtranduythanh@gmail.com>
2017-02-13 09:10:16 +0700

    rebuilding site Mon Feb 13 09:10:16 ICT 2017

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
-cp content/post/static/* public/static/*
 
 # Go To Public folder
 cd public
